787404ffb4ed65d0fb4f232687f24e2738db77c9
Luis Boullosa <luis.boullosa@evobio.eu>
2021-05-27 14:41:14 +0200

    Server is realoaded automatically

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import data from './data.js';
 
 const app = express();
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 }) ;
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server at httpL//localhost:3000');
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
 });
