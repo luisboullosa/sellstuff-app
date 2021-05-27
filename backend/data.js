c39ef14f31d5604bb97b323caf4f662e094b02ca
Luis Boullosa <luis.boullosa@evobio.eu>
2021-05-27 14:49:15 +0200

    COmmented out lightspeed-specific code

---
+++ backend/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name:'Turkoois hanger "blob"',
+            category:'Hangers',
+            image:'../images/Product-1-1.jpg',
+            price:120,
+            countInStock: 10,
+            brand:'LiAtelier',
+            rating:4.5,
+            numReviews:10,
+            description:'Mooie turkoois hanger',
+        },
+        {
+            _id: '2',
+            name:'Turkoois hanger "medalla"',
+            category:'Hangers',
+            image:'../images/Product-2-1.jpg',
+            price:150,
+            countInStock: 10,
+            brand:'LiAtelier',
+            rating:4.5,
+            numReviews:9,
+            description:'Mooie turkoois hanger',
+            
+        },
+        {
+            _id: '3',
+            name:'Turkoois hanger',
+            category:'Hangers',
+            image:'../images/Product-1-1.jpg',
+            price:110,
+            countInStock: 0,
+            brand:'LiAtelier',
+            rating:4.5,
+            numReviews:10,
+            description:'Mooie turkoois hanger',
+            
+        },        
+        {
+            _id: '4',
+            name:'Turkoois hanger',
+            category:'Hangers',
+            image:'../images/Product-1-1.jpg',
+            price:130,
+            countInStock: 11,
+            brand:'LiAtelier',
+            rating:4.5,
+            numReviews:10,
+            description:'Mooie turkoois hanger',
+            
+        },
+        {
+            _id: '5',
+            name:'Turkoois hanger',
+            category:'Hangers',
+            image:'../images/Product-1-1.jpg',
+            price:140,
+            countInStock: 10,
+            brand:'LiAtelier',
+            rating:4.5,
+            numReviews:10,
+            description:'Mooie turkoois hanger',
+
+            
+        },
+        
+    ]
+
+};
+export default data;
